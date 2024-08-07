ccd2c5cc4697a2f1713ea0228bb3e52c87664ce9
ankur srivastava <ankur.srivastava@techchefz.com>
2024-08-07 23:09:53 +0530

    navigate button

--- src/component/NavigateOnClickBtn/index.jsx
+++ src/component/NavigateOnClickBtn/index.jsx
@@ -4,7 +4,7 @@ const NavigateOnClickBtn =({navigateBtnText, onClick})=>{
     return(
         <>
         <h1 className="text-center text-3xl font-semibold">Navigate Button On Click</h1>
-        <button onClick={onClick} className="bg-dodgerblue p-4">{navigateBtnText}</button>
+        <button onClick={onClick} className="bg-dodgerblue py-4 px-8 rounded-lg font-semibold text-white">{navigateBtnText}</button>
         </>
     )
 }
--- src/pages/UseState/index.jsx
+++ src/pages/UseState/index.jsx
@@ -7,27 +7,32 @@ import NavigateOnClickBtn from '../../component/NavigateOnClickBtn'
 const UseState = () => {
 
   const navigate = useNavigate()  // Navigate button on Click
-  
+
   const [hide, setHide] = useState(true)
   const [toggle, setToggle] = useState(true)
   return (
-    <section className="max-w-[1320px] mx-auto bg-whiteLightSecondary py-20 px-4">
-      <h1>This is UseState Page Show & Hide{hide && ("This Text to be Hide")}</h1>
+    <>
+      <section className="max-w-[1320px] mx-auto bg-whiteLightSecondary py-20 px-4">
+        <h1>This is UseState Page Show & Hide{hide && ("This Text to be Hide")}</h1>
 
-      <h1>Toggle button Show / Hide , {toggle && ("Toggle Button 'Show' , 'hide' text change")}</h1>
+        <h1>Toggle button Show / Hide , {toggle && ("Toggle Button 'Show' , 'hide' text change")}</h1>
 
-      <button className="py-3 px-9 rounded text-lg font-semibold mt-3 bg-orange-500 text-white" onClick={() => (setHide(false))}>Hide</button>
-      <button className="py-3 px-9 rounded text-lg font-semibold mt-3 bg-orange-500 ml-3 text-white" onClick={() => (setHide(true))}>Show</button>
-      <button className="py-3 px-9 rounded text-lg font-semibold mt-3 bg-slate-600 ml-3 text-white" onClick={() => (setToggle(!toggle))}>
-        Toggle / {
-          toggle ? 'Hide' : 'Show'
-        }
-      </button>
-      <NavigateOnClickBtn
-      navigateBtnText = 'Ankur'
-      onClick={()=>navigate('/')}
-      />
-    </section>
+        <button className="py-3 px-9 rounded text-lg font-semibold mt-3 bg-orange-500 text-white" onClick={() => (setHide(false))}>Hide</button>
+        <button className="py-3 px-9 rounded text-lg font-semibold mt-3 bg-orange-500 ml-3 text-white" onClick={() => (setHide(true))}>Show</button>
+        <button className="py-3 px-9 rounded text-lg font-semibold mt-3 bg-slate-600 ml-3 text-white" onClick={() => (setToggle(!toggle))}>
+          Toggle / {
+            toggle ? 'Hide' : 'Show'
+          }
+        </button>
+      </section>
+
+      <section className="max-w-[1320px] mx-auto my-14">
+        <NavigateOnClickBtn
+          navigateBtnText='Navigate Home Page'
+          onClick={() => navigate('/')}
+        />
+      </section>
+    </>
   )
 }
 
