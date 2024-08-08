162fc908fc7531452f48c0ff5417082300103a0e
ankur srivastava <ankur.srivastava@techchefz.com>
2024-08-08 17:47:17 +0530

    rating

--- src/component/CustomerStarRating/index.jsx
+++ src/component/CustomerStarRating/index.jsx
@@ -5,22 +5,30 @@ const CustomerStarRating = () => {
     const [hover, setHover] = useState(0)
     return (
         <>
-            <div className="flex gap-4">
-                {
-                    [1, 2, 3, 4, 5].map((star, index) => (
-                        <button
-                            className='text-5xl text-gray'
-                            onClick={() => setRating(star)}
-                            onMouseOver={() => setHover(star)}
-                            onMouseLeave={() => setHover(rating)}
+            <div className="flex flex-col">
+                <div className="">
+                    {
+                        [1, 2, 3, 4, 5].map((star, index) => (
+                            <button key={index}
+                                className='text-5xl text-gray'
+                                onClick={() => setRating(star)}
+                                onMouseOver={() => setHover(star)}
+                                onMouseLeave={() => setHover(rating)}
 
-                        >
-                            <span className={`text-5xl  ${star <= (hover || rating) ? 'text-yellow-400' : 'text-gray-400'}`}>
-                                &#9733;
-                            </span>
-                        </button>
-                    ))
-                }
+                            >
+                                <span className={`text-5xl  ${star <= (hover || rating) ? 'text-yellow-400' : 'text-gray-400'}`}>
+                                    &#9733;
+                                </span>
+                            </button>
+                        ))
+                    }
+                    {/* Show hover number or rating */}
+                    <div className="mt-4 text-lg font-semibold">
+                        {hover > 0 ? `Selected Rating: ${hover} / 5` : `Selected Rating: ${rating} / 5`}
+                    </div>
+                </div>
+
+
             </div>
         </>
     )
